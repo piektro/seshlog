27a7b0b1edc65a15d26700805b5faad32db7b2b1
Pedro Yribarren <p.yribarren@gmail.com>
2016-12-07 00:26:58 -0800

    Add users with device

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-end
 
-config.action_mailer.default_url_options = { :host => 'seshlog.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'seshlog.heroku.com' }
+end
