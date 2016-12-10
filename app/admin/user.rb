b3daf1328612ab1431c42125645777e925d5686b
Pedro Yribarren <p.yribarren@gmail.com>
2016-12-10 10:17:28 -0800

    Set up admin index and users

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -13,5 +13,22 @@ ActiveAdmin.register User do
 #   permitted
 # end
 
+permit_params :email, :admin
 
+	form do |f|
+		f.semantic_errors
+		f.inputs do
+			f.input :email
+			f.input :admin
+		end
+		f.actions
+	end
+
+	index do
+		id_column
+		column :email
+		column :created_at
+		column :admin
+		actions
+	end
 end
