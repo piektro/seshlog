c270a793c074f699f96f588192b1eca315a310c6
Pedro Yribarren <p.yribarren@gmail.com>
2016-12-26 22:44:16 -0800

    CSS for index and validation

--- app/models/privsesh.rb
+++ app/models/privsesh.rb
@@ -1,3 +1,5 @@
 class Privsesh < ApplicationRecord
   belongs_to :user
+
+  validates :note, length: {maximum: 100}
 end
--- app/models/sharesesh.rb
+++ app/models/sharesesh.rb
@@ -1,3 +1,9 @@
 class Sharesesh < ApplicationRecord
   belongs_to :user
+
+  validates :when, presence: true
+  validates :where, presence: true
+  validates :subject, presence: true
+  validates :yoursesh, presence: true
+  	
 end
