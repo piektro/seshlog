dc07ec0652f101c8ab0db66d98d6695f87741e5c
Pedro Yribarren <p.yribarren@gmail.com>
2016-12-29 01:58:51 -0800

    sggdf

---
+++ config/initializers/aws.rb
@@ -0,0 +1,16 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Aws.config.update({
+  region: 'oregon',
+  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+})
+
+S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
