703ba20e131cc800b1d175d9f7df4a0003d96283
Pedro Yribarren <p.yribarren@gmail.com>
2016-12-29 01:21:00 -0800

    Found error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,10 +90,10 @@ Rails.application.configure do
 config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('seshimg'),
-    access_key_id: ENV.fetch('AKIAI6LVRVTHJS2YAYKQ'),
-    secret_access_key: ENV.fetch('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'),
-    s3_region: ENV.fetch('oregon'),
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
   }
 }
 end
