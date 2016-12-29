a958d97e1620521dee5b441a53c03d0fb7e895ed
Pedro Yribarren <p.yribarren@gmail.com>
2016-12-29 02:02:04 -0800

    dgdfg

--- app/controllers/shareseshes_controller.rb
+++ app/controllers/shareseshes_controller.rb
@@ -56,7 +56,4 @@ class ShareseshesController < ApplicationController
       params.require(:sharesesh).permit(:when, :where, :subject, :yoursesh, :image)
     end
 
-    def set_s3_direct_post
-      @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
-    end
 end
