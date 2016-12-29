dac9111105c214b6344fbf599dffb388468fa633
Pedro Yribarren <p.yribarren@gmail.com>
2016-12-29 04:19:52 -0800

    Finally

--- app/models/sharesesh.rb
+++ app/models/sharesesh.rb
@@ -4,7 +4,7 @@ class Sharesesh < ApplicationRecord
   validates :when, presence: true
   validates :where, presence: true
   validates :subject, presence: true
-  validates :yoursesh, presence: true
+  validates :subject, presence: true, length: {maximum: 30}
 
   has_attached_file :image, styles: {card: "334x189#", cardshow: "710x402#"}, default_url: ->(attachment) { ActionController::Base.helpers.image_path('missing.png', digest: false) }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
