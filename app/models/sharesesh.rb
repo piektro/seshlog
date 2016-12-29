7bae930c879aef8a3c30156c7517f72727e2fbd4
Pedro Yribarren <p.yribarren@gmail.com>
2016-12-29 02:26:15 -0800

    Deleted default route

--- app/models/sharesesh.rb
+++ app/models/sharesesh.rb
@@ -6,6 +6,6 @@ class Sharesesh < ApplicationRecord
   validates :subject, presence: true
   validates :yoursesh, presence: true
 
-  has_attached_file :image, styles: {card: "334x189#", cardshow: "710x402#"}, default_url: ->(attachment) { ActionController::Base.helpers.asset_path('assets/:style/missing.png', digest: false) }
+  has_attached_file :image, styles: {card: "334x189#", cardshow: "710x402#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
